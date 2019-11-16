d91c882856faa0e9d2faca07a1dc2cd4dec47366
kevorkkeheian <kevork.keheian@codgram.com>
2019-11-16 09:38:53 +0200

    Finished code challenge in 6-Dates

--- 4 - String variables/code_challenge.py
+++ 4 - String variables/code_challenge.py
@@ -4,3 +4,12 @@
 # Make sure you have a space between first and last name
 # Make sure the first letter of first name and last name is uppercase
 # Make sure the rest of the name is lowercase
+
+
+firstName = input("Please enter your first name: ")
+lastName = input("Please eneter your last name: ")
+
+fullName = firstName.capitalize() + " " + lastName.capitalize()
+
+print(fullName)
+
--- 6 - Dates/code_challenge.py
+++ 6 - Dates/code_challenge.py
@@ -1,4 +1,27 @@
-# print today's date
-# print yesterday's date
-# ask a user to enter a date
+
+
 # print the date one week from the date entered
+
+from datetime import datetime  #To extract date and time functions
+from datetime import timedelta #To extract functions such as timedelta
+
+today = datetime.now()
+
+
+# print today's date
+print(today)
+
+# print yesterday's date
+one_day = timedelta(days = 1)
+yesterday = today - one_day
+print("yesterday was " + str(yesterday))
+
+
+# ask a user to enter a date
+birthday = input("Please enter your birthday (dd/mm/yyyy): ")
+birthday_day = datetime.strptime(birthday, '%d/%m/%Y')
+print("Birhtday: " + str(birthday_day))
+
+# print the date one week from the date entered
+seven_days = timedelta(days = 7)
+print(birthday_day - seven_days)
